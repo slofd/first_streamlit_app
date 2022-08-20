262b44cdb79ba2755748b8d94c388410399a62c7
slofd <111581128+slofd@users.noreply.github.com>
2022-08-20 19:59:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit
 
 streamlit.title("My parents healthy dinner")
 
-streamlit.header('Breakfast menu')
 
-streamlit.text('menu1')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
