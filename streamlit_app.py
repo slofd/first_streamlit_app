6cac5a420ae1a7c48444f4ecb916e4a9fa479654
slofd <slofd@qq.com>
2022-08-23 21:24:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,6 +55,7 @@ def get_fruit_load_list():
 
 
 if streamlit.button('Get Fruit Load List'):
+    streamlit.header("Fruit load list:")
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
