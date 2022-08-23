39aaabb012b479e7b8c96ec2b1002544e9969fb6
slofd <111581128+slofd@users.noreply.github.com>
2022-08-23 19:15:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,27 +19,35 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
 
+# get data from snowflake
+streamlit.header("Fruityvice Fruit Advice!")
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select fruit_name from fruit_load_list")
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION(),current_database(), current_schema(),current_role()")
 my_data_row = my_cur.fetchall()
-streamlit.header("Load fruit list:")
-streamlit.dataframe(my_data_row)
+my_data_row =my_data_row.set_index('fruit_name');
+
+# fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruits_selected=streamlit.multiselect("What fruit would you like information about?", list(my_data_row.index),['Avocado','Strawberries'])
+
+streamlit.write('The user entered ', fruits_selected)
+
+# show api response data
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("select fruit_name from fruit_load_list")
+# # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION(),current_database(), current_schema(),current_role()")
+# my_data_row = my_cur.fetchall()
+# streamlit.header("Load fruit list:")
+# streamlit.dataframe(my_data_row)
 
 
 
